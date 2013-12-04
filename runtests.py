298a41189c40fd28ab5e935d98ca74df8d35a94a
priyaank <choubey.priyank@gmail.com>
2013-12-05 01:49:21 +0530

    updated runtest

--- runtests.py
+++ runtests.py
@@ -9,14 +9,14 @@ from task.worker_pool import WorkerPool
 
 class WorkerPool_TC(TestCase):
 
-    def test_11init_worker_pass(self):
+    def test_11_init_process_worker_pass(self):
         try:
             WorkerPool(default_start=False)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
 
-    def test_12worker_size_pass(self):
+    def test_12_process_worker_size_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False)
             pool_size = len(worker.processors)
@@ -24,7 +24,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_13_worker_start_pass(self):
+    def test_13_process_worker_start_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False, wait_time=0)
             worker.start_processing()
@@ -32,7 +32,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_14queue_len_pass(self):
+    def test_14_process_queue_len_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False, wait_time=0)
             is_empty = worker.task_is_empty()
@@ -40,7 +40,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_15task_add_pass(self):
+    def test_15_process_task_add_pass(self):
         try:
             worker = WorkerPool(default_start=False, wait_time=0)
             worker.add_tasks([(5,3),(6,4)])
@@ -49,7 +49,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_16task_processing(self):
+    def test_16_process_task_processing(self):
         try:
             worker = WorkerPool(wait_time=0)
             worker.add_tasks([(5,3)])
@@ -61,14 +61,14 @@ class WorkerPool_TC(TestCase):
             self.fail("Unexpected failure")
 
 
-    def test_21init_worker_pass(self):
+    def test_21_threaded_init_worker_pass(self):
         try:
             WorkerPool(default_start=False, is_threaded=True)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
 
-    def test_22_worker_size_pass(self):
+    def test_22_threaded_worker_size_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False, is_threaded=True)
             pool_size = len(worker.processors)
@@ -76,7 +76,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_23_worker_start_pass(self):
+    def test_23_threaded_worker_start_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False, wait_time=0, is_threaded=True)
             worker.start_processing()
@@ -84,7 +84,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_24queue_len_pass(self):
+    def test_24_threaded_queue_len_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False, wait_time=0, is_threaded=True)
             is_empty = worker.task_is_empty()
@@ -92,7 +92,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_25task_add_pass(self):
+    def test_25_threaded_task_add_pass(self):
         try:
             worker = WorkerPool(default_start=False, wait_time=0, is_threaded=True)
             worker.add_tasks([(5,3),(6,4)])
@@ -101,7 +101,7 @@ class WorkerPool_TC(TestCase):
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_26task_processing(self):
+    def test_26_threaded_task_processing(self):
         try:
             worker = WorkerPool(wait_time=0, is_threaded=True)
             worker.add_tasks([(5,3)])
