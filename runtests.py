2a44a4d54220080dd3ee836b3fb8a3c5423b70c4
priyaank <choubey.priyank@gmail.com>
2013-12-05 01:10:34 +0530

    Updated runtest to test thread and process worker queue

--- runtests.py
+++ runtests.py
@@ -4,62 +4,113 @@
 import time
 import unittest2 as unittest
 from unittest2 import TestCase
-from task.thread import WorkerPool
+from task.worker_pool import WorkerPool
+
 
 class WorkerPool_TC(TestCase):
 
-    def test_1init_worker_pass(self):
+    def test_11init_worker_pass(self):
         try:
             WorkerPool(default_start=False)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
 
-    def test_2_worker_size_pass(self):
+    def test_12worker_size_pass(self):
         try:
             worker = WorkerPool(pool_size=1, default_start=False)
-            pool_size = worker.pool_size()
+            pool_size = len(worker.processors)
             self.assertEqual(pool_size,1)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_3_worker_start_pass(self):
+    def test_13_worker_start_pass(self):
         try:
-            worker = WorkerPool(pool_size=1, default_start=False)
+            worker = WorkerPool(pool_size=1, default_start=False, wait_time=0)
             worker.start_processing()
             worker.stop_all()
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_queue_len_pass(self):
+    def test_14queue_len_pass(self):
         try:
-            worker = WorkerPool(pool_size=1, default_start=False)
-            queue_size = worker.task_in_queue()
-            self.assertEqual(queue_size,0)
+            worker = WorkerPool(pool_size=1, default_start=False, wait_time=0)
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,True)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_task_add_pass(self):
+    def test_15task_add_pass(self):
         try:
-            worker = WorkerPool(default_start=False)
+            worker = WorkerPool(default_start=False, wait_time=0)
             worker.add_tasks([(5,3),(6,4)])
-            queue_size = worker.task_in_queue()
-            self.assertEqual(queue_size,2)
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,False)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
-    def test_task_processing(self):
+    def test_16task_processing(self):
         try:
-            worker = WorkerPool()
+            worker = WorkerPool(wait_time=0)
             worker.add_tasks([(5,3)])
             worker.stop_all()
             time.sleep(4)
-            queue_size = worker.task_in_queue()
-            self.assertEqual(queue_size,0)
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,True)
         except RuntimeError as e:
             self.fail("Unexpected failure")
 
 
+    def test_21init_worker_pass(self):
+        try:
+            WorkerPool(default_start=False, is_threaded=True)
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
+
+
+    def test_22_worker_size_pass(self):
+        try:
+            worker = WorkerPool(pool_size=1, default_start=False, is_threaded=True)
+            pool_size = len(worker.processors)
+            self.assertEqual(pool_size,1)
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
+
+    def test_23_worker_start_pass(self):
+        try:
+            worker = WorkerPool(pool_size=1, default_start=False, wait_time=0, is_threaded=True)
+            worker.start_processing()
+            worker.stop_all()
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
+
+    def test_24queue_len_pass(self):
+        try:
+            worker = WorkerPool(pool_size=1, default_start=False, wait_time=0, is_threaded=True)
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,True)
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
+
+    def test_25task_add_pass(self):
+        try:
+            worker = WorkerPool(default_start=False, wait_time=0, is_threaded=True)
+            worker.add_tasks([(5,3),(6,4)])
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,False)
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
+
+    def test_26task_processing(self):
+        try:
+            worker = WorkerPool(wait_time=0, is_threaded=True)
+            worker.add_tasks([(5,3)])
+            worker.stop_all()
+            time.sleep(4)
+            is_empty = worker.task_is_empty()
+            self.assertEqual(is_empty,True)
+        except RuntimeError as e:
+            self.fail("Unexpected failure")
 
 
 
