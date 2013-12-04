d5827224838ebd4beedb8bdc7fd049801003a040
priyaank <choubey.priyank@gmail.com>
2013-12-05 01:33:01 +0530

    Updated try_it with new test run for threaded worker pool and process based worker pool

--- try_it.py
+++ try_it.py
@@ -2,7 +2,7 @@
 # Priyaank Choubey - mail@priyaank.com
 
 import time
-from task.thread import WorkerPool
+from task.worker_pool import WorkerPool
 
 #
 # Executes if the program is started normally, not if imported
@@ -10,37 +10,21 @@ from task.thread import WorkerPool
 
 SAMPLE_INPUT_TASKS = [ (5,5),(10,4),(78,5),(87,2),(65,4),(10,10),(65,2),(88,95),(44,55),(33,3) ]
 def sample_run1():
-    test_worker = WorkerPool()
-    print "Tasks in Queue before adding Sample Tasks {}".format(test_worker.task_in_queue())
+    test_worker = WorkerPool(is_threaded=True)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    print "Tasks in Queue after adding Sample Tasks {}".format(test_worker.task_in_queue())
     time.sleep(3)
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
     #worker_test.add_tasks(inputlist_ori)
     #worker_test.stop_all()
 
 def sample_run2():
     test_worker = WorkerPool()
-    print "Tasks in Queue before adding Sample Tasks {}".format(test_worker.task_in_queue())
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    print "Tasks in Queue after adding Sample Tasks {}".format(test_worker.task_in_queue())
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
     time.sleep(8)
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
     #worker_test.stop_all()
 
 def sample_run3():
-    test_worker = WorkerPool()
-    print "Tasks in Queue before adding Sample Tasks {}".format(test_worker.task_in_queue())
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    print "Tasks in Queue after adding Sample Tasks {}".format(test_worker.task_in_queue())
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
-    time.sleep(8)
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
-    print "Stopped Tasks Processing."
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
+    test_worker = WorkerPool(is_threaded=True, wait_time=0, pool_size=2, max_try=1)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
@@ -49,9 +33,27 @@ def sample_run3():
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
     test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    print "Tasks in Queue {}".format(test_worker.task_in_queue())
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+
+
+def sample_run4():
+    test_worker = WorkerPool(max_try=1, wait_time=0, pool_size=2)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
 
 if __name__ == '__main__':
     #sample_run1()
     #sample_run2()
+    print "Try running Threaded WorkerPool"
     sample_run3()
+    time.sleep(25)
+    print "Try running Process WorkerPool"
+    sample_run4()
+
