30ff1192d4e3f1c47e3970371911b36d29245d25
priyaank <choubey.priyank@gmail.com>
2013-12-09 03:54:27 +0530

    updated try_it, and deleted file

--- try_it.py
+++ try_it.py
@@ -2,58 +2,28 @@
 # Priyaank Choubey - mail@priyaank.com
 
 import time
-from task.worker_pool import WorkerPool
+from task_queue.task import Task
+from task_queue.core import WorkerPool
 
 #
 # Executes if the program is started normally, not if imported
 #
 
-SAMPLE_INPUT_TASKS = [ (5,5),(10,4),(78,5),(87,2),(65,4),(10,10),(65,2),(88,95),(44,55),(33,3) ]
-def sample_run1():
-    test_worker = WorkerPool(is_threaded=True)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    time.sleep(3)
-    #worker_test.add_tasks(inputlist_ori)
-    #worker_test.stop_all()
 
-def sample_run2():
-    test_worker = WorkerPool()
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    time.sleep(8)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    #worker_test.stop_all()
+class MultiplyTask(Task):
+    """docstring for MultiplyTask"""
+    def __init__(self, a, b):
+        super(Task, self).__init__()
+        self.a = a
+        self.b = b
 
-def sample_run3():
-    test_worker = WorkerPool(is_threaded=True, wait_time=0, pool_size=2, max_try=1)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+    def run(self):
+        print self.a*self.b
 
 
-def sample_run4():
-    test_worker = WorkerPool(max_try=1, wait_time=0, pool_size=2)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
-    test_worker.add_tasks(SAMPLE_INPUT_TASKS)
+a = MultiplyTask(10,20)
 
-if __name__ == '__main__':
-    #sample_run1()
-    #sample_run2()
-    print "Try running Threaded WorkerPool"
-    sample_run3()
-    time.sleep(25)
-    print "Try running Process WorkerPool"
-    sample_run4()
+SAMPLE_INPUT_TASKS = [ (5,5),a ]
 
+test_worker = WorkerPool(is_thread=True)
+test_worker.add_tasks(SAMPLE_INPUT_TASKS)
